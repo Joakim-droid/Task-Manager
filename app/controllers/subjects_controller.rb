98534c44eb3a43025ba25f79ef7c68c24149198c
Joakim Hunskaar <joakim.borge.hunskar@gmail.com>
2020-10-25 09:01:20 +0100

    added default value for completed and add methods to subjects_controller

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,9 +1,20 @@
 class SubjectsController < ApplicationController
-    def index; end
-    def show; end
-    def new; end
-    def create; end
+    def index 
+      @subjects = Subject.where(user: current_user)
+    end
+    def new
+      @subject = Subject.new
+    end
+    def create
+      @subject = Subject.create(subject_params)
+    end
     def edit; end
     def update; end
     def destroy; end
+
+    private
+
+    def subject_params
+      params.require(:subject).permit(:title)
+    end
 end
